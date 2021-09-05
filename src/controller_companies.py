f5214dfc474dc506a5f34df464eed1f66353ffdd
Artur Henrique do Nascimento Souza <artur.souza@fcamara.com.br>
2021-09-04 23:55:32 -0300

    refactor and add any validations

---
+++ src/controller_companies.py
@@ -0,0 +1,94 @@
+from models import Companies
+from flask import  Response, json
+from create_response import create_response
+
+from cpf_cnpj.cpf_cnpj import Cnpj
+from blabla import db
+
+
+class CompanieController:
+    @staticmethod
+    def list():
+        companies_class = Companies.query.all()
+
+        companies_class_json = [company.to_json() for company in companies_class]
+
+        return create_response(200, "companies", companies_class_json)
+
+    @staticmethod
+    def find(cnpj):
+
+        obj_cnpj = Cnpj(cnpj).format()
+
+        company_obj = Companies.query.filter_by(cnpj=obj_cnpj).first()
+    
+        if(company_obj is None):
+            return create_response(404,"Company",{})
+
+        company_json = company_obj.to_json()
+
+        return create_response(200, "Company", company_json)
+
+    @staticmethod
+    def create(body):
+        obj_cnpj = Cnpj(body["cnpj"])
+
+        if(not obj_cnpj.validate()):
+            return create_response(400,"Error",{"field":"Invalid cnpj"},"No Create")
+
+        cnpj_formated = obj_cnpj.format()
+
+        if(Companies.query.filter_by(cnpj=cnpj_formated).first()):
+            return create_response(400,"Error",{"field":"Exist another company with this CNPJ"},"No Create")
+
+        try:
+            company = Companies(nome=body["nome"],
+                                cnpj=cnpj_formated, senha=body["senha"])
+
+            db.session.add(company)
+            db.session.commit()
+            return create_response(201, "Company", company.to_json(), "Create Sucesseful")
+        except Exception as e:
+            print(e)
+            return create_response(400, "Company", {}, "Error in create company")
+    
+    @staticmethod
+    def update_by_cnpj(cnpj,body):
+        obj_cnpj = Cnpj(cnpj).format()
+
+        company_obj= Companies.query.filter_by(cnpj=obj_cnpj).first()
+
+        if(company_obj is None):
+            return create_response(404,"Company",{})
+
+        try:
+            if('nome' in body):
+                company_obj.nome = body["nome"]
+            if('senha' in body):
+                company_obj.senha = body["senha"]
+            
+            db.session.add(company_obj)
+            db.session.commit()
+
+            return create_response(200, "Company", company_obj.to_json(), "Update Sucesseful")
+        except Exception as e:
+            print(e)
+            return create_response(400, "Company", {}, "Error in update company")
+
+
+    @staticmethod
+    def delete(cnpj):
+        obj_cnpj = Cnpj(cnpj).format()
+
+        company_obj = Companies.query.filter_by(cnpj=obj_cnpj).first()
+
+        if(company_obj is None):
+            return Response(status=404)
+        
+        try:
+            db.session.delete(company_obj)
+            db.session.commit()
+            return create_response(204,"Company",company_obj.to_json(),"Sucessiful deleted")
+        except Exception as e:
+            print(e)
+            return create_response(400, "Company", {}, "Error in delete company")
