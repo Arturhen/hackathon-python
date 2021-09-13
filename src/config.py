fc9089fae2b51d7c112fcae3861f504afbfe3c04
Artur Henrique do Nascimento Souza <artur.souza@fcamara.com.br>
2021-09-13 16:49:04 -0300

    make .env and connection production

--- src/config.py
+++ src/config.py
@@ -2,9 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_cors import CORS
 
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+ 
+load_dotenv()
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+ 
+SECRET_KEY = os.getenv("SECRET_KEY")
+DATABASE_URI = os.getenv("DATABASE_URI")
+DEV_DATABASE_URI = os.getenv("DEV_DATABASE_URI")
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:root@db:5432/hackathon'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 CORS(app)
 
 db = SQLAlchemy(app)
